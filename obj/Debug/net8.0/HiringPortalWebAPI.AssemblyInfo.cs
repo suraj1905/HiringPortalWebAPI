df8691f9b99271631c6e8ac09336d99dd9fa53cc
suraj1905 <surajsharma9378@gmail.com>
2024-05-20 16:18:10 +0530

    Test Commit

--- obj/Debug/net8.0/HiringPortalWebAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/HiringPortalWebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HiringPortalWebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2d36d63dab6de34ff34cdf65eb5271040cedc22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1848eaede9bc876ba2ee2c4c106146a1980a09a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("HiringPortalWebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HiringPortalWebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
